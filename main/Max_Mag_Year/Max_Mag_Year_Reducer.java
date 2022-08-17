093cc065b9e47af0a7f4c5f5c27f189dfa99645d
XuejiaoDong <73553375+XuejiaoDong@users.noreply.github.com>
2022-08-17 17:06:19 -0400

    Create Max_Mag_Year_Reducer.java

---
+++ main/Max_Mag_Year/Max_Mag_Year_Reducer.java
@@ -0,0 +1,28 @@
+package com.mycompany.maxmin_mag_year;
+
+import java.io.IOException;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import java.io.IOException;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ *
+ * @author mywatercolor
+ */
+public class Max_Mag_Year_Reducer extends Reducer <Text, FloatWritable, Text,FloatWritable>{
+    
+
+    public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException{
+    // called once for each key
+      float maxValue = Float.MIN_VALUE;
+      for(FloatWritable val : values)
+        maxValue= Math.max(maxValue,val.get());
+      
+      context.write(key, new FloatWritable(maxValue));//return key,reduceVal
+    }
+}
