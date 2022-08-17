04fc5ec4c839614fd6a33bb4632099e7166513bf
XuejiaoDong <73553375+XuejiaoDong@users.noreply.github.com>
2022-08-17 17:08:06 -0400

    Create NumberReducer.java

---
+++ main/NumberOfEarthquakes/NumberReducer.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.numberofearthquakes;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+
+
+public class NumberReducer extends Reducer <Text, IntWritable, Text, IntWritable>{
+
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key,Iterable <IntWritable> values,Context context) throws IOException,InterruptedException
+        {
+            int sum = 0;
+            for (IntWritable val : values)
+            {
+                sum = sum + val.get();
+            }
+            result.set(sum);
+            context.write(key,result);
+        }
+}
